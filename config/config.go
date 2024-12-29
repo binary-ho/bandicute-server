6d5c16b43b9a2e34a6359cf8cc5186c4e0e8d8fe
binary-ho <dfghcvb11@naver.com>
2024-12-30 00:50:00 +0900

    feat: config를 관리하는 방식 변경

---
+++ config/config.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+)
+
+var instance *Config
+
+type Config struct {
+	Server struct {
+		Port    int    `yaml:"port"`
+		Env     string `yaml:"env"`
+		Address string `yaml:"address"`
+	} `yaml:"server"`
+
+	Database struct {
+		BaseURL string `yaml:"base_url"`
+		Key     string `yaml:"key"`
+	} `yaml:"database"`
+
+	GitHub struct {
+		Token string `yaml:"token"`
+	} `yaml:"github"`
+
+	OpenAI struct {
+		APIKey string `yaml:"api_key"`
+	} `yaml:"openai"`
+
+	Logging struct {
+		Level string `yaml:"level"`
+	} `yaml:"logging"`
+}
+
+const (
+	configPath = "config/config.yml"
+)
+
+func GetInstance() *Config {
+	if instance == nil {
+		return loadConfig()
+	}
+	return instance
+}
+
+func loadConfig() *Config {
+	instance = &Config{}
+	if err := loadYAML(configPath, instance); err != nil {
+		panic(err)
+	}
+	return instance
+}
+
+func loadYAML(filename string, cfg *Config) error {
+	file, err := os.ReadFile(filepath.Clean(filename))
+	if err != nil {
+		return err
+	}
+	return yaml.Unmarshal(file, cfg)
+}
